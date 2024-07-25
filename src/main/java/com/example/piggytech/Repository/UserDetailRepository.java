165e2888451064a01f81a37c1584da5a16a8e028
bigboss09niel <ronielmadrigal.rm@gmail.com>
2024-07-26 01:50:23 +0800

    under processing...........

--- src/main/java/com/example/piggytech/Repository/UserDetailRepository.java
+++ src/main/java/com/example/piggytech/Repository/UserDetailRepository.java
@@ -4,12 +4,9 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.piggytech.Model.Role;
 import com.example.piggytech.Model.UserAuth;
 import com.example.piggytech.Model.UserDetail;
 
-public interface UserDetailRepository  extends  JpaRepository<UserDetail, Long>{
-
-    Optional<Role> findByUserAuth(UserAuth userAuth);
-
+public interface UserDetailRepository extends JpaRepository<UserDetail, Long> {
+    Optional<UserDetail> findByUserAuth(UserAuth userAuth);
 }
